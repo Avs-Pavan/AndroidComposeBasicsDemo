25be7a53e3d76b878cb777cedea62c3c964da41d
A.v.s.Pavan <avspavan1234@gmail.com>
2023-10-30 11:48:36 -0500

    ConstraintLayoutDemo added

---
+++ app/src/main/java/com/kevin/androidcomposebasicsdemo/constraintlayout/ConstraintLayoutDemo.kt
@@ -0,0 +1,81 @@
+package com.kevin.androidcomposebasicsdemo.constraintlayout
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+
+class ConstraintLayoutDemo {
+
+    @Preview
+    @Composable
+    fun ConstraintDemo() {
+
+        ConstraintLayout(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Cyan)
+        ) {
+            val (r1, r2, r3, r4, r5,r6,r7,r8) = createRefs()
+
+            Box(modifier = Modifier
+                .background(Color.Black)
+                .size(50.dp)
+                .constrainAs(r1) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                })
+            Box(modifier = Modifier
+                .background(Color.Black)
+                .size(50.dp)
+                .constrainAs(r2) {
+                    top.linkTo(r1.bottom)
+                    start.linkTo(r1.end)
+                })
+            Box(modifier = Modifier
+                .background(Color.Black)
+                .size(50.dp)
+                .constrainAs(r3) {
+                    top.linkTo(r2.bottom)
+                    start.linkTo(r2.end)
+                })
+            Box(modifier = Modifier
+                .background(Color.Black)
+                .size(50.dp)
+                .constrainAs(r4) {
+                    top.linkTo(r3.bottom)
+                    start.linkTo(r3.end)
+                })
+            Box(modifier = Modifier
+                .background(Color.Black)
+                .size(50.dp)
+                .constrainAs(r5) {
+                    top.linkTo(parent.top)
+                    end.linkTo(parent.end)
+                })
+            Box(modifier = Modifier
+                .background(Color.Black)
+                .size(50.dp)
+                .constrainAs(r6) {
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                })
+            Box(modifier = Modifier
+                .background(Color.Black)
+                .size(50.dp)
+                .constrainAs(r7) {
+                    bottom.linkTo(parent.bottom)
+                    end.linkTo(parent.end)
+                })
+
+
+        }
+    }
+
+}
